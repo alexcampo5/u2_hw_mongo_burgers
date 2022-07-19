6b9ea62731b8dd45e144d024490d3a8d21b1e7dc
Alex Campo <abrettc@gmail.com>
2022-07-19 15:47:18 -0500

    finished homework

--- answers.js
+++ answers.js
@@ -1,32 +1,63 @@
 // create 5 burgers (at least 3 should be beef)
-
+db.burgers.insertMany([
+  {
+    name: '',
+    protein: 'beef',
+    hasCheese: true,
+    toppings: ['lettuce', 'onion']
+  },
+  {
+    name: '',
+    protein: 'veggie',
+    hasCheese: false,
+    toppings: ['lettuce', 'onion', 'tomato']
+  },
+  {
+    name: '',
+    protein: 'turkey',
+    hasCheese: false,
+    toppings: ['lettuce', 'ketchup']
+  },
+  { name: '', protein: 'beef', hasCheese: true, toppings: [] },
+  { name: '', protein: 'beef', hasCheese: true, toppings: ['onions', 'mayo'] }
+])
 // find all the burgers
-
+db.burgers.find({})
 // show just the meat of each burger
-
+db.burgers.find({}, { protein: 1 })
 // show just the toppings of each burger
-
+db.burgers.find({}, { toppings: 1 })
 // show everything but the cheese
-
+db.burgers.find({}, { protein: 1, toppings: 1 })
 // find all the burgers with beef
-
+db.burgers.find({ protein: 'beef' })
 // find all the burgers that are not beef
-
+db.burgers.find({ protein: { $ne: 'beef' } })
 // find the first burger with cheese
 
 // find one and update the first burger with cheese to have a property of 'double cheese'
-
+db.burgers.updateOne(
+  { hasCheese: true },
+  { $set: { doubleCheese: true }, $currentDate: { lastModified: true } }
+)
 // find the burger you updated to have double cheese
-
+db.burgers.find({ doubleCheese: true })
 // find and update all the beef burgers to be 'veggie'
-
+db.burgers.updateMany(
+  { protein: 'beef' },
+  { $set: { protein: 'veggie' }, $currentDate: { lastModified: true } }
+)
 // delete one of your veggie burgers
 // WRONG - dELETES ALL : db.burger.remove({meat: 'veggie'})
+db.burgers.deleteOne({ protein: 'veggie' })
 
 // drop the collection
 //Expected Output
 //true
-
+db.burgers.updateMany(
+  {},
+  { $set: { dropped: true }, $currentDate: { lastModified: true } }
+)
 // drop the database
 //Expected Output
 // {
@@ -48,4 +79,4 @@
 // add a topping of 'eggs' to all the beef burgers
 //note since this db is 'reset' there should be no veggie burgers, all beef burgers should still be intact
 
-//Add a price to each burger, start with $5.00 for each burger 
+//Add a price to each burger, start with $5.00 for each burger
